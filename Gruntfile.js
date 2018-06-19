a3629eb0879574084610afced55a4969c39dd03d
laserps <mosaklang@gmail.com>
2018-06-19 08:52:55 +0700

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'assets/css/inspire.css' : 'assets/sass/inspire.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: 'assets/sass/**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
